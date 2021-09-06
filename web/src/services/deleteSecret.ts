e765645800e2fb3f3861238ca6591f7007827e1e
elysee15 <elyseebleu1@gmail.com>
2021-09-06 16:19:55 +0000

    fix: destroy secured secret

--- web/src/services/deleteSecret.ts
+++ web/src/services/deleteSecret.ts
@@ -1,6 +1,18 @@
 import { AxiosPromise, AxiosRequestConfig } from "axios";
 import client from "config";
 
-export default function deleteSecret(token: string, config: AxiosRequestConfig = {}): AxiosPromise {
+export default function deleteSecret(
+	token: string,
+	password?: string | null,
+	config: AxiosRequestConfig = {}
+): AxiosPromise {
+	if (password) {
+		return client.delete(`/secrets/${token}`, {
+			headers: {
+				Authorization: `Bearer ${password}`,
+				"Access-Control-Request-Headers": "Authorization"
+			}
+		});
+	}
 	return client.delete(`/secrets/${token}`, config);
 }
